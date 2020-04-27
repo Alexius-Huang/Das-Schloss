2cf31ae0db9b9570ed5532f8468a2b1e72e5786a
Maxwell-Alexius <alexius.huang@gmail.com>
2020-04-28 00:53:21 +0800

    Prototype lesson menu

--- src/containers/Lessons.tsx
+++ src/containers/Lessons.tsx
@@ -1,11 +1,60 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { FaGrinAlt, FaFlag, FaCheck } from 'react-icons/fa';
+import 'scss/pages/Lessons.scss';
 
 const Lessons: React.FC = () => {
+  const history = useHistory();
+
   return (
     <div className="page page__lessons">
-      Lessons
+      <div className="card-group">
+        <h1 className="card-group__title"><FaFlag /> One Step Forward</h1>
+
+        <ul className="card-group__cards">
+          <li className="card card--conversation">
+            <button className="card__wrapper-button">
+              <span className="card__img-wrapper">
+                <FaGrinAlt />
+              </span>
+              <span className="card__title">Wilkommen!</span>
+            </button>
+          </li>
+        </ul>
+      </div>
+
+      <div className="card-group">
+        <h1 className="card-group__title"><FaCheck /> Fundamentals I</h1>
+
+        <ul className="card-group__cards">
+          <li className="card card--grammer">
+            <button className="card__wrapper-button">
+              <span className="card__img-wrapper">
+                <FaGrinAlt />
+              </span>
+              <span className="card__title">Personalpronomen</span>
+            </button>
+          </li>
+          <li className="card card--grammer">
+            <button className="card__wrapper-button">
+              <span className="card__img-wrapper">
+                <FaGrinAlt />
+              </span>
+              <span className="card__title">Das Nomen</span>
+            </button>
+          </li>
+          <li className="card card--grammer">
+            <button className="card__wrapper-button">
+              <span className="card__img-wrapper">
+                <FaGrinAlt />
+              </span>
+              <span className="card__title">Das Verb</span>
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default Lessons;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { IconContext } from 'react-icons';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <IconContext.Provider value={{ className: 'react-icon' }}>
+      <App />
+    </IconContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
